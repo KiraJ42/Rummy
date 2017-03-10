c19589da180b141bdd7e9c649528510c9ac84217
KiraJ42 <spockian42@gmail.com>
2017-03-10 10:50:09 -0500

    Create Card.java

---
+++ Card.java
@@ -0,0 +1,18 @@
+public class Card 
+{
+	//card ranks and 
+	public static final int HEART 1;
+	public static final int DIAMOND 2;
+	public static final int SPADE 3; 
+	public static final int CLUB 4; 
+	
+	public static final int JACK 11;
+	public static final int QUEEN 12;
+	public static final int KING 13;
+	public static final int ACE_HIGH 14;
+	public static final int ACE_LOW 1;
+	
+	public int color;
+	public int suit;
+	public int rank;
+}
