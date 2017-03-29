52caa756d40e7fba25bf6ffa8523c40b548444b5
KiraJ42 <spockian42@gmail.com>
2017-03-29 10:13:22 -0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,1 +1,12 @@
-//Player class
+import java.util.ArrayList;
+
+public class Player {
+
+    protected String name;
+    public ArrayList<Card> hand = new ArrayList<Card>();
+
+    @Override
+    public String toString() {
+        return name + "\n" + "\t" + hand.toString();
+    }
+}
