9a8ff59cf904ca12336a3b83e250b11731517e1e
KiraJ42 <spockian42@gmail.com>
2017-04-02 11:28:54 -0400

    Update Deck.java

--- Deck.java
+++ Deck.java
@@ -1,27 +1,39 @@
 import java.util.ArrayList;
 import java.util.Collections;
 
+/**
+ * Created by Owner on 3/28/2017.
+ */
 public class Deck {
     private ArrayList<Card> deck;
     private ArrayList<Card> discard;
 
-    Deck()
+    Deck(int deckNum)
     {
         deck = new ArrayList<Card>();
         discard = new ArrayList<Card>();
 
-        //adding Jokers
         int jokerSuit = 4;
         int jokerRank = 13;
 
-        deck.add(new Card(jokerSuit, jokerRank));
-        deck.add(new Card(jokerSuit, jokerRank));
+        if(deckNum == 1) {
+            deck.add(new Card(jokerSuit, jokerRank));
+            deck.add(new Card(jokerSuit, jokerRank));
+        }
 
-        for(int suit = 0; suit < 4; suit++)
+        if(deckNum == 2)
         {
-            for(int rank = 0; rank < 13; rank++)
-            {
-                deck.add(new Card(suit, rank));
+            deck.add(new Card(jokerSuit, jokerRank));
+            deck.add(new Card(jokerSuit, jokerRank));
+            deck.add(new Card(jokerSuit, jokerRank));
+            deck.add(new Card(jokerSuit, jokerRank));
+        }
+
+        for(int i = 0; i < deckNum; i++) {
+            for (int suit = 0; suit < 4; suit++) {
+                for (int rank = 0; rank < 13; rank++) {
+                    deck.add(new Card(suit, rank));
+                }
             }
         }
     }
@@ -37,6 +49,19 @@ public class Deck {
         Card ret = deck.get(0);
         deck.remove(0);
         return ret;
+    }
+
+    public void Deal(int hand, Player[] players)
+    {
+        for(Player x : players)
+        {
+            int i = 0;
+            while(i < hand)
+            {
+                x.hand.add(takeCard());
+                i++;
+            }
+        }
     }
 
     public void Discard(Card card)
