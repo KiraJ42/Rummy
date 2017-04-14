d772e6326d8cc457a900556700b7bf7170b9c13a
KiraJ42 <spockian42@gmail.com>
2017-04-14 12:50:37 -0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -5,6 +5,10 @@ public class Player {
     protected String name;
     public ArrayList<Card> hand = new ArrayList<Card>();
 
+    Player(String n){
+        this.name = n;
+    }
+    
     @Override
     public String toString() {
         return name + "\n" + "\t" + hand.toString();
