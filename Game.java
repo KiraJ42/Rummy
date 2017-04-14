b13b9e67f182e11a2cdf304f7f48ca1aa2655f7c
KiraJ42 <spockian42@gmail.com>
2017-04-14 13:08:01 -0400

    Update Game.java

--- Game.java
+++ Game.java
@@ -5,8 +5,7 @@ import javax.swing.*;
  */
 public class Game {
 
-    private static int playerNum;
-    Game(){
+    Game() {
         JFrame window = new JFrame("Rummy");
 
         rummyGUI content = new rummyGUI();
@@ -14,23 +13,34 @@ public class Game {
         window.pack();
         window.setResizable(true);
         window.setSize(500, 500);
-        window.setLocation(100,100);
+        window.setLocation(100, 100);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setVisible(true);
 
         String[] types = {"Rummy", "Gin Rummy", "Dummy Rummy"};
-        String name = JOptionPane.showInputDialog(window, "Enter Your Name:", "Player Name",JOptionPane.PLAIN_MESSAGE);
+        String name = JOptionPane.showInputDialog(window, "Enter Your Name:", "Player Name", JOptionPane.PLAIN_MESSAGE);
         Player player = new Player(name);
-        String gameType = (String) JOptionPane.showInputDialog(window, "Which Rummy game would you like to play?", "Choose Game",JOptionPane.QUESTION_MESSAGE, null, types,types[0]);
-        String num = JOptionPane.showInputDialog(window, "How many players in your game?");
-        int p = Integer.parseInt(num);
-        int nums;
-        if(gameType.equals("Dummy Rummy"))
-            nums = 2;
-        else
-            nums = 1;
-        System.out.println(nums);
-        Deck d1 = new Deck(nums);
+        String gameType = (String) JOptionPane.showInputDialog(window, "Which Rummy game would you like to play?", "Choose Game", JOptionPane.QUESTION_MESSAGE, null, types, types[0]);
+
+        Integer[] playerNum = new Integer[3];
+        int deckNum;
+        if (gameType.equals("Dummy Rummy")){
+            deckNum = 2;
+            for(int i = 0; i < 3; i++){
+                playerNum[i] = i+3;
+            }
+        }
+        else {
+            deckNum = 1;
+            for(int i = 0; i < 3; i++){
+                playerNum[i] = i+2;
+            }
+        }
+
+
+        Integer num = (Integer) JOptionPane.showInputDialog(window, "How many players in your game?", "Players",JOptionPane.QUESTION_MESSAGE, null, playerNum, playerNum[0]);
+
+        Deck d1 = new Deck(deckNum);
         d1.shuffle();
 
         //d1.Deal(11, p);
