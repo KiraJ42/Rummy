60fc16ddf680053d1caec919df688c1ebb988e02
KiraJ42 <spockian42@gmail.com>
2017-04-14 12:49:26 -0400

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,4 +1,5 @@
-import java.util.Scanner;
+import javax.swing.*;
+
 /**
  * Created by Owner on 3/28/2017.
  */
@@ -6,32 +7,33 @@ public class Game {
 
     private static int playerNum;
     Game(){
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter the number of players for your game: ");
-        playerNum =  input.nextInt();
+        JFrame window = new JFrame("Rummy");
 
-        Player[] players = new Player[playerNum];
-        for(int p = 0; p < playerNum; p++)
-        {
-            players[p] = new Player();
-        }
+        rummyGUI content = new rummyGUI();
+        window.setContentPane(content);
+        window.pack();
+        window.setResizable(true);
+        window.setSize(500, 500);
+        window.setLocation(100,100);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setVisible(true);
 
-        for(int i = 0; i < playerNum; i++){
-            System.out.print("Enter Player " + (i+1) + " name: ");
-            players[i].name = input.next();
-        }
-
-        Deck d1 = new Deck(2);
+        String[] types = {"Rummy", "Gin Rummy", "Dummy Rummy"};
+        String name = JOptionPane.showInputDialog(window, "Enter Your Name:", "Player Name",JOptionPane.PLAIN_MESSAGE);
+        Player player = new Player(name);
+        String gameType = (String) JOptionPane.showInputDialog(window, "Which Rummy game would you like to play?", "Choose Game",JOptionPane.QUESTION_MESSAGE, null, types,types[0]);
+        String num = JOptionPane.showInputDialog(window, "How many players in your game?");
+        int p = Integer.parseInt(num);
+        int nums;
+        if(gameType.equals("Dummy Rummy"))
+            nums = 2;
+        else
+            nums = 1;
+        System.out.println(nums);
+        Deck d1 = new Deck(nums);
         d1.shuffle();
 
-        d1.Deal(11, players);
-
-        for(int t = 0; t < playerNum; t++)
-        {
-            System.out.println(players[t]);
-        }
-
-        System.out.println(d1);
+        //d1.Deal(11, p);
     }
     public static void main(String[] args)
     {
