0aec1626a9aae9edfbce55f172e16ccda2479ff8
PrairieChienne <rea.j.wilson@gmail.com>
2017-04-18 11:11:30 -0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -3,10 +3,19 @@ import java.util.ArrayList;
 public class Player {
 
     protected String name;
+    protected int score;
+    
     public ArrayList<Card> hand = new ArrayList<Card>();
 
     Player(String n){
         this.name = n;
+        score = 0;
+    }
+    
+    //takes in an int and adds it to the player's total score
+    public int addScore(int i)
+    {
+        score += i;
     }
     
     @Override
