979e53d66051a7302c32de290a8688bf33b75d60
PrairieChienne <rea.j.wilson@gmail.com>
2017-04-18 16:55:58 -0400

    Update Rummy.java

--- Rummy.java
+++ Rummy.java
@@ -1,5 +1,41 @@
 import java.util*
 
 public class Rummy extends Rules
-{
+{ 
+    Rummy(int numP, String n)
+    {
+        super(numP, n);
+        
+        deck = new Deck(1);
+        
+    }
+    public boolean victoryCheck()
+    {
+        for(Player p : players)
+        {
+            if(p.getScore() >= 500)
+              return true;
+        }
+        
+        return false;
+    }
+    
+    public void deal()
+    {
+        deck.Deal(7, players);  
+    }
+    
+    //tabulate scores and empty hands for a new round
+    public void endRound()
+    {
+        for(Player p : players)
+        {
+            p.addTotalScore(p.getLaysScore() - p.getHandScore());
+            p.clear(deck);
+        }    
+    }
+    
+    //increases current
+    public abstract void nextTurn();
+    
 }
