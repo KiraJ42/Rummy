fb1b553f06528fd9b4f1b07a4d4cd449b672f5b5
PrairieChienne <rea.j.wilson@gmail.com>
2017-04-18 15:48:04 -0400

    Create Lays.java

---
+++ Lays.java
@@ -0,0 +1,12 @@
+import java.util*
+
+public abstract class Lays()
+{
+    ArrayList<Card> lay;
+    int score;
+    
+    public abstract void addLay();
+    
+    public int getScore(){return score;}
+    
+}
