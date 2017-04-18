0589737960ba074340614346e97863fb2da928ca
PrairieChienne <rea.j.wilson@gmail.com>
2017-04-18 16:39:45 -0400

    Update Set.java

--- Set.java
+++ Set.java
@@ -1,5 +1,34 @@
 import java.util.*;
 
 public class Set extends Lays(){
+    //CAN ONLY BE USED AFTER AN ISVALID CHECK OF C
+    Set(ArrayList<Card> cards)
+    {
+        lay.addAll(cards);
+        score = 0;
+        
+        for(Card c : lay)
+        {
+            score += c.getValue();
+        }
+    }
+  
+    public static boolean isValid(ArrayList<Card> cards)
+    {
+        String rank;
+      
+        if(cards.size() < 3)
+            return false;
+        
+        rank = cards.get(0).getRank();
+        
+        for(Card c : cards)
+        {
+            if(!rank.equals(c.getRank()))   
+              return false;
+        }
+      
+        return true;
+    }
     
 }
