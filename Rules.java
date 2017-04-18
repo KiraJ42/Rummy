ef3c0e00f0daf9d97b7cd4e289760fe0395dd8c6
PrairieChienne <rea.j.wilson@gmail.com>
2017-04-18 14:18:31 -0400

    Create Rules.java

---
+++ Rules.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public abstract class Rules 
+{
+    ArrayList<Player> players;
+    
+    Rules(int numP)
+    {
+        for(int i = 0; i < numP; i++)
+        {
+        }
+    }
+  
+}
