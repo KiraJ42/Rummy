23f512d024dc95d23adb94fd572248b5e2ee6e92
KiraJ42 <spockian42@gmail.com>
2017-04-18 15:59:22 -0400

    Update Lays.java

--- Lays.java
+++ Lays.java
@@ -5,7 +5,6 @@ public abstract class Lays()
     ArrayList<Card> lay;
     int score;
     
-    public abstract void addLay();
     public static void isValid();
     
     public int getScore(){return score;}
