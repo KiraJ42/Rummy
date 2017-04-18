3d5d6c5ed4d37eea45b202846e34d7fc512fdda9
PrairieChienne <rea.j.wilson@gmail.com>
2017-04-18 11:41:56 -0400

    Update Card.java

--- Card.java
+++ Card.java
@@ -9,11 +9,20 @@ public class Card {
 
     public String suit;
     public String rank;
-
-    Card(int suit, int rank)
+    
+    private ImageIcon front;
+    private ImageIcon back;
+    private int x;
+    private int y;
+    
+    private boolean up;
+    
+    Card(int suit, int rank, ImageIcon f, ImageIcon b)
     {
         this.rank = RANKS[rank];
         this.suit = SUITS[suit];
+        front = f;
+        back = b;
     }
 
     public String toString()
@@ -32,5 +41,32 @@ public class Card {
     {
         return suit;
     }
+    
+    public void draw(Graphics g, Component c) 
+    {
+        if(up)
+            front.paintIcon(c, g, x, y);
+        else
+            back.paintIcon(c, g, x, y);
+    }
+    
+    public int getHeight(){return front.getIconHeight();}
+    public int getWidth(){return front.getIconWidth();}
+    
+    public boolean contains(int a, int b)
+    {
+        return ( a > x && a < (x + getWidth()) && b > y && b < (y + getHeight()));
+    }
+    
+    public int getY(){return y;}     
+    public int getX(){return x;}
+    
+    public void moveTo(int a, int b)
+    {
+        x = a;
+        y = b;
+    }
+
+    
 }
 
