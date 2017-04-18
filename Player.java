be2b59bcff643fd1e669c71f080da088a6a4b2fb
PrairieChienne <rea.j.wilson@gmail.com>
2017-04-18 16:08:00 -0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -6,9 +6,10 @@ public class Player {
     protected int score;
     
     public ArrayList<Card> hand = new ArrayList<Card>();
-
+    public ArrayList<Lays> lays = new ArrayList<Lays>();
+    
     Player(String n){
-        this.name = n;
+        name = n;
         score = 0;
     }
     
