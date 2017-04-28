d65e8badcea8f5d717263048b25bb18ff2ca1d16
KiraJ42 <spockian42@gmail.com>
2017-04-28 13:39:52 -0400

    images work

--- Card.java
+++ Card.java
@@ -1,13 +1,22 @@
+import javax.swing.ImageIcon;
+import java.net.URL;
+import java.awt.Graphics;
+import java.awt.Component;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.io.*;
+import java.awt.*;
+import javax.swing.*;
+
 /**
  * Created by Owner on 3/28/2017.
  */
 public class Card {
 
     
-    String bPath = "card back red.png";
-    URL bURL = cldr.getResource(bPath);
-    public static final ImageIcon back =  new ImageIcon(bURL);
-   
+    //static String bPath = "card back red.png";
+    //URL bURL = cldr.getResource(bPath);
+    //public static final ImageIcon Cardback =  new ImageIcon(bPath);
     
     //card ranks and
     public static final String[] RANKS = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Joker"};
@@ -15,22 +24,57 @@ public class Card {
 
     public String suit;
     public String rank;
-    public String intRank;
+    public int intRank;
     
-    private ImageIcon front;
-    private ImageIcon back;
+    public ImageIcon front;
+    static public ImageIcon back;
     private int x;
     private int y;
     
     private boolean up;
     
-    Card(int suit, int rank, ImageIcon f, int iRank)
+    Card(int s, int r, int iRank)
     {
-        rank = RANKS[rank];
-        suit = SUITS[suit];
+        rank = RANKS[r];
+        suit = SUITS[s];
         intRank = iRank;
-        front = f;
-        back = b;
+        //front = f;
+
+        back = getImage("images/gold_crown.png");
+        front = getImage("images/" + rank + suit + ".png");
+    }
+
+    public ImageIcon getImage(String path){
+        try {
+            InputStream is = new BufferedInputStream(new FileInputStream(path));
+            Image image = ImageIO.read(is);
+           Image img = image.getScaledInstance(140,200, Image.SCALE_SMOOTH);
+            return new ImageIcon(img);
+        }catch(Exception e){
+            System.out.println("File not found");
+        }
+        return new ImageIcon();
+    }
+
+   static public void changeBack(String p){
+        String path = "images/gold_crown.png";
+
+        if(p.equals("gold"))
+            path = "images/gold_crown.png";
+        if(p.equals("red"))
+            path = "images/card back red.png";
+        if(p.equals("blue"))
+            path = "images/blue.png";
+
+       try {
+           InputStream is = new BufferedInputStream(new FileInputStream(path));
+           Image image = ImageIO.read(is);
+           Image img = image.getScaledInstance(140,200, Image.SCALE_SMOOTH);
+           back = new ImageIcon(img);
+       }catch(Exception e){
+           System.out.println("File not found");
+       }
+       back = new ImageIcon();
     }
 
     public String toString()
