a8304c019ba06df383abc8185349e16719696f5e
PrairieChienne <rea.j.wilson@gmail.com>
2017-04-30 15:36:54 -0400

    Update Computer.java

--- Computer.java
+++ Computer.java
@@ -2,11 +2,30 @@ import java.util.ArrayList;
 
 public class Computer extends Player{
 
-    Computer(int i){
+    Computer(int i)
+    {
         name = "Player " + i;
         totalScore = 0;
         details = new PlayerInfo(this);
     }
     
+    public void TakeTurn(deck d)
+    {
+        Card discard = d.getDiscard();
+        bool getDiscard;
+        
+        for(Lay l : allLays)
+        {
+            if()
+        }
+        
+        for(Card c : hand)
+        {
+            //insert crying here
+        }
+        
+        if(getDiscard)
+            
+    }
     
 }
