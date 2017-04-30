dbf14b6c7ed4569f943f24813235594af549be6b
KiraJ42 <spockian42@gmail.com>
2017-04-30 13:52:51 -0400

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,7 +1,13 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 
+
+/**
+ * Created by Owner on 3/28/2017.
+ */
 public class Game {
 
     public static String gameType;
@@ -10,6 +16,8 @@ public class Game {
     public static JFrame window;
     static playerHandGUI playerHand;
     static JLabel discard;
+    static Player player;
+    //static JPanel center;
 
     Game() {
         window = new JFrame("Rummy");
@@ -25,32 +33,31 @@ public class Game {
 
         String[] types = {"Rummy", "Gin Rummy"};
         String name = JOptionPane.showInputDialog(window, "Enter Your Name:", "Welcome to Rummy!", JOptionPane.PLAIN_MESSAGE);
-        Player player = new Player(name);
+        player = new Player(name);
         gameType = (String) JOptionPane.showInputDialog(window, "Which Rummy game would you like to play?", "Choose Game", JOptionPane.QUESTION_MESSAGE, null, types, types[0]);
         Integer[] playerNum = {2, 3, 4};
         Integer num = (Integer) JOptionPane.showInputDialog(window, "How many players in your game?", "Players",JOptionPane.QUESTION_MESSAGE, null, playerNum, playerNum[0]);
 
-
-        JPanel center = new JPanel();
         JPanel scores = new JPanel();
+        JPanel center = new JPanel();
+        JPanel centerBtns = new JPanel();
+        centerBtns.setBackground(new Color(130,50,40));
+        center.setLayout(new BoxLayout(center, BoxLayout.Y_AXIS));
+        JPanel centerCard = new JPanel();
+        centerCard.setBackground(new Color(130,50,40));
         center.setBackground( new Color(130,50,40) );
         scores.setBackground( new Color(130,50,40) );
 
         Card.backImg = Card.getImage(Card.back);
         JLabel cardDeck = new JLabel(Card.backImg);
-        cardDeck.addMouseListener(new MouseAdapter(){
+        cardDeck.addMouseListener(new MouseAdapter() {
             @Override
-            public void mouseClicked(MouseEvent e){
+            public void mouseClicked(MouseEvent e) {
                 pickCard();
             }
         });
 
-        PlayerInfo play = new PlayerInfo();
-        scores.add(play);
-        center.add(cardDeck);
-        window.add(scores, BorderLayout.NORTH);
-
-        window.add(center, BorderLayout.CENTER);
+        centerCard.add(cardDeck);
 
         d = new Deck();
 
@@ -62,23 +69,29 @@ public class Game {
         for(int i = 0; i < (num-1); i++)
             players.add(new Player("Player" + (i+2)));
 
+        for(Player p : players) {
+            System.out.println(p.name);
+            scores.add(p.details);
+        }
+        window.add(scores, BorderLayout.NORTH);
+
         d.Deal(7, players);
 
         playerHand = new playerHandGUI(player);
 
         window.getContentPane().add(playerHand, BorderLayout.SOUTH);
-        
+
         cardButtons buttonPanel = new cardButtons(this);
 
-        scores.add(buttonPanel);
+       // scores.add(buttonPanel);
 
         ArrayList<Card> disc = d.getDiscard();
         Card r = disc.get(disc.size()-1);
         discard = new JLabel(r.frontImg);
         discard.addMouseListener(new MouseAdapter() {
             @Override
-            public void mouseClicked(MouseEvent e){
-                Card cd = d.takeDiscard():
+            public void mouseClicked(MouseEvent e) {
+                Card cd = d.takeDiscard();
                 player.hand.add(cd);
                 playerHand.add(cd);
                 updateDiscard();
@@ -86,32 +99,31 @@ public class Game {
                 window.repaint();
             }
         });
-        center.add(discard);
+        centerCard.add(discard);
+        centerBtns.add(buttonPanel);
+        center.add(centerCard);
+        center.add(centerBtns);
+
+        window.add(center, BorderLayout.CENTER);
 
         window.validate();
         window.repaint();
 
-        
-        /*if(gameType.equals("Rummy"))*/
-            //rule = new Rummy(num, name);
-        
-        /*else if(gameType.equals("GinRummy"))
-            rule = new GinRummy(num, name);*/
-        
-       /* while(!rule.victoryCheck())
-        {
-            rule.deal();
-            
-            while(!rule.nextTurn());
 
-            rule.endRound();
-        }*/
-        //d1.Deal(11, p);
+     /*   if(gameType.equals("Rummy"))
+            rule = new Rummy(num, name);
+        else
+            rule = new GinRummy(num, name);
+
+       while(!rule.victoryCheck()) {
+           rule.deal();
+
+           while (!rule.nextTurn()) ;
+
+           rule.endRound();
+       }*/
     }
-    public static void main(String[] args)
-    {
-        Game game = new Game();
-    }
+    public static void main(String[] args) {Game game = new Game();}
 
     public void updateDiscard(){
         if(d.getDiscard().size() > 0) {
@@ -119,6 +131,17 @@ public class Game {
             Container parent = discard.getParent();
             parent.remove(discard);
             discard = new JLabel(x.frontImg);
+            discard.addMouseListener(new MouseAdapter() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+                    Card cd = d.takeDiscard();
+                    player.hand.add(cd);
+                    playerHand.add(cd);
+                    updateDiscard();
+                    window.validate();
+                    window.repaint();
+                }
+            });
             parent.add(discard);
             window.validate();
             window.repaint();
@@ -132,16 +155,12 @@ public class Game {
             window.repaint();
         }
     }
-    
-    public class pickCard implements ActionListener{
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            Player p = players.get(0);
-            Card newC = d.takeCard();
-            p.hand.add(newC);
-            playerHand.add(newC);
-            window.validate();
-            window.repaint();
-        }
+    public void pickCard() {
+        Player p = players.get(0);
+        Card newC = d.takeCard();
+        p.hand.add(newC);
+        playerHand.add(newC);
+        window.validate();
+        window.repaint();
     }
 }
