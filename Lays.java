8cf0f0e5f544a3e6ee0baab688144a4dfbf4dd25
KiraJ42 <spockian42@gmail.com>
2017-04-30 14:08:41 -0400

    Update Lays.java

--- Lays.java
+++ Lays.java
@@ -1,12 +1,12 @@
-import java.util*
+import java.util.*;
 
-public abstract class Lays()
-{
+public abstract class Lays{
+
     ArrayList<Card> lay;
     int score;
-    
-    public static boolean isValid();
-    
+
+    public static boolean isValid(){return true;}
+
     public int getScore(){return score;}
-    
+    public static ArrayList<Card> getCards(){return new ArrayList<Card>();}
 }
