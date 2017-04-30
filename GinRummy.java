1a3f39f521a0e68623e3d92db89e9ec7a20479c6
KiraJ42 <spockian42@gmail.com>
2017-04-30 14:03:27 -0400

    Update GinRummy.java

--- GinRummy.java
+++ GinRummy.java
@@ -1,5 +1,26 @@
-import java.util*
+import java.util.*;
 
 public class GinRummy extends Rules
 {
+    GinRummy(int numP, String n){
+        super(numP, n);
+    }
+    public void deal(){
+
+    }
+
+    public boolean nextTurn()
+    {
+        return true;
+    }
+
+    @Override
+    public boolean victoryCheck() {
+        return false;
+    }
+
+    @Override
+    public void endRound() {
+
+    }
 }
