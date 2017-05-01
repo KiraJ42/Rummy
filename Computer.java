9473884470fbecc9dd1a83f910ec94b60cde87ba
PrairieChienne <rea.j.wilson@gmail.com>
2017-05-01 18:22:30 -0400

    Update Computer.java

--- Computer.java
+++ Computer.java
@@ -1,38 +1,62 @@
 import java.util.ArrayList;
 
 public class Computer extends Player{
+    
+    ArrayList<ArrayList<Card>> runs = new  ArrayList<ArrayList<Card>>;
+    ArrayList<ArrayList<Card>> sets = new  ArrayList<ArrayList<Card>>;
 
     Computer(int i)
     {
         name = "Player " + i;
         totalScore = 0;
         details = new PlayerInfo(this);
+        sets.ensureCapacity(14);
+        runs.ensureCapacity(4);
+        
+        //set all the run cards to jokers
+        for(ArrayList<Card> a : runs)
+        {
+            for(int i = 0; i < 14; i++)
+            {
+                a.add(new Card(4, 13, 14));
+            }
+        }
     }
+    
     
     public void TakeTurn(deck d)
     {
         Card discard = d.getDiscardCard();
         bool getDiscard = false;
         
+        OrderHands();
+        
         for(Lay l : allLays)
         {
             if(l.addCard(discard))
             {
-               hand.add(d.takeDiscard());
+               addCard(d.takeDiscard());
                getDiscard = true;
             }
         }
         
         if(!getDiscard)
         {
-            for(Card c : hand)
+            if(makesSet(discard))
             {
-                //insert crying here
+               addCard(d.takeDiscard());
+               getDiscard = true;
+            }
+            
+            else if(makesRun(discard))
+            {
+               addCard(d.takeDiscard());
+               getDiscard = true;
             }
         }
         
         if(!getDiscard)
-            hend.add(d.takeCard());
+            hand.add(d.takeCard());
         
         for(Card c : hand)
         {
@@ -41,6 +65,45 @@ public class Computer extends Player{
             
     }
     
+    public void addCard(Card c)
+    {
+        hand.add(c);
+        sets[c.getIntRank].add(c);
+        runs[c.getIntSuit].set([c.getIntRank], c);
+    }
     
+    public void play(Card c)
+    {
+        totalScore += c.getValue();
+        hand.remove(c);
+        
+        
+    }
+    
+    public boolean makesSet(Card c)
+    {   
+        if(sets.[c.getIntRank].size() + 1 >= 3)
+            return true;
+        
+        return false     
+    }
+    
+    public boolean makesRun(Card c)
+    {   
+        boolean valid = false;
+        if(c.getIntRank >= 2)
+            valid = runs[c.getIntSuit][c.getIntRank - 1].getIntRank != 14;
+        
+        if(c.getIntRank >= 3 && !valid)
+            valid = runs[c.getIntSuit][c.getIntRank - 2].getIntRank != 14;
+        
+        if(c.getIntRank <= 12 && !valid)
+            valid = runs[c.getIntSuit][c.getIntRank + 1].getIntRank != 14;
+        
+        if(c.getIntRank <= 11 && !valid)
+            valid = runs[c.getIntSuit][c.getIntRank + 2].getIntRank != 14;
+        
+        return valid;     
+    }
     
 }
