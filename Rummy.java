fc7f49cc6bee5f39f7b11b8ed7e1a81aa239a3d5
PrairieChienne <rea.j.wilson@gmail.com>
2017-05-04 00:44:40 -0400

    Update Rummy.java

--- Rummy.java
+++ Rummy.java
@@ -2,13 +2,12 @@ import java.util.*;
 
 public class Rummy extends Rules
 {
-    Rummy(int numP, String n)
-    {
-        super(numP, n);
-        
-        deck = new Deck();
-        
+
+    public Rummy(Game g) {
+        super(g);
     }
+
+
     public boolean victoryCheck()
     {
         for(Player p : players)
@@ -32,13 +31,50 @@ public class Rummy extends Rules
         {
             p.addTotalScore(p.getLaysScore() - p.getHandScore());
             p.clear(deck);
+            
         }    
+                
+        Player.allLays.clear();
+        
+        
+        deck.discardClear();
     }
     
     //increases current
-    public boolean nextTurn()
+    public int playRound()
     {
-        return true;
+        boolean winner = false;
+        int i = 0;
+        
+        //deal();
+        
+        while(!winner)
+        {
+            for(i = 0; i < players.size() && !winner; i++)
+            {                                            
+                if(Player.turns == 0)
+                {
+                    winner = players.get(0).TakeTurn();
+                    players.get(0).isTurn = false;
+                }
+                
+                else{
+                    winner = ((Computer)players.get(i)).TakeTurn(deck);
+                    game.updateDiscard();
+                }
+                
+                if(Player.turns == players.size())
+                    Player.turns = 0;
+                 
+            }
+            
+
+        }
+        
+        endRound();
+        return i;
     }
+
+
     
 }
