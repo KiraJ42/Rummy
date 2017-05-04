4c33933179eaa7566a81a2d7796912fb80e1675e
PrairieChienne <rea.j.wilson@gmail.com>
2017-05-04 11:57:12 -0400

    Update Set.java

--- Set.java
+++ Set.java
@@ -1,55 +1,146 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 
-public class Set extends Lays{
-    //CAN ONLY BE USED AFTER AN ISVALID CHECK OF C
+public class Player extends Thread{
 
-    Set(ArrayList<Card> cards)
-    {
-        lay = new ArrayList<>();
-        lay.addAll(cards);
-        score = 0;
-        
-        for(Card c : lay)
-        {
-            score += c.getValue();
-        }
-    }
-  
-    public static boolean isValid(ArrayList<Card> cards)
-    {
-        String rank;
-      
-        if(cards.size() < 3)
-            return false;
-        
-        rank = cards.get(0).getRank();
-        
-        for(Card c : cards)
-        {
-            if(!rank.equals(c.getRank()))   
-              return false;
-        }
-      
-        return true;
-    }
-
-    @Override
-    public boolean addCard(Card c)
-    {
-        boolean valid = false;
-        lay.add(c);
-        
-        if(isValid(lay))
-            valid = true;
-   
-        
-        lay.remove(lay.size() - 1);
-        return valid;      
+    public String name;
+    protected int totalScore;
+    public boolean isTurn;
+    public boolean discarded;
+    public boolean drawn;
+    public PlayerInfo details;
+    public static int turns;
+    public static Game g;
+    
+    public ArrayList<Card> hand = new ArrayList<Card>();
+    public ArrayList<Card> checkLay = new ArrayList<Card>();
+    public ArrayList<Lays> lays = new ArrayList<Lays>();
+    public static ArrayList<Lays> allLays = new ArrayList<Lays>();
+    public ArrayList<Lays> ScoredLays = new ArrayList<Lays>();
+    
+    Player(String n, Game g){
+        name = n;
+        totalScore = 0;
+        details = new PlayerInfo(this);
+        isTurn = false;
+        discarded = false;
+        turns = 0;
+        g = g;
     }
     
+    //takes in an int and adds it to the player's total score
+    public int addTotalScore(int i)
+    {
+        totalScore += i;
+        return totalScore;
+    }
+    
+    public int getTotalScore(){
+
+
+        totalScore = totalScore + getLaysScore();
+        return totalScore;
+    }
+    
+    public int getLaysScore()
+    {
+        int score = 0;
+        
+        for(Lays l : lays)
+        {
+            /*for(Card c : l.lay)
+            {
+                for(Lays la : ScoredLays)
+                {
+                    if(!la.lay.contains(c))
+                    {
+                        score += l.getScore();
+                        ScoredLays.add(l);
+                        allLays.add(l);
+                    }
+                }
+            }*/
+            
+            score += l.getScore();
+            ScoredLays.add(l);
+            
+            if(!l.single)
+                allLays.add(l);
+            
+        }
+
+        lays.clear();
+        return score;
+    }
+    public int getHandScore()
+    {
+        int score = 0;
+        
+        for(Card c : hand)
+        {
+            score -= c.getValue();
+        }
+        
+        return score;
+    }
+    
+    public void clear(Deck deck)
+    {
+        for(Card c : hand)
+        {
+            deck.Discard(c);
+        }
+        hand.clear();
+        
+        for(Lays l : ScoredLays)
+        {
+            for(Card c : l.getCards())
+            {
+                    deck.Discard(c);
+            }
+        }
+        lays.clear();
+    }
+
+    public boolean TakeTurn()
+    {
+        isTurn = true;
+        discarded = false;
+        drawn = false;
+      
+        //while(turns == 0)
+            
+        while(!discarded)
+        {
+            
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Player.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+        }
+        
+        if(hand.isEmpty())
+        {
+            
+            JOptionPane.showMessageDialog(null, "You have won! Congratulations!");
+            System.exit(0);
+            
+           
+        }
+            
+        return hand.isEmpty();
+    }
+    
+    public void addCard(Card c)
+    {
+        hand.add(c);
+    }
     @Override
     public String toString() {
-        Card c = lay.get(0);
-        return  c.getRank() + ", ";
+        return name + "\n" + "\t" + hand.toString();
     }
 }
